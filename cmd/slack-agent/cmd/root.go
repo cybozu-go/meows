7edadaab724e0318d765cea3646b25d393b75a4a
Masayuki Ishii <masa213f@gmail.com>
2021-04-23 07:33:27 +0000

    Remove redundant error output
    Signed-off-by: Masayuki Ishii <masa213f@gmail.com>

--- cmd/slack-agent/cmd/root.go
+++ cmd/slack-agent/cmd/root.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -19,7 +18,6 @@ var rootCmd = &cobra.Command{
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
 		os.Exit(1)
 	}
 }
