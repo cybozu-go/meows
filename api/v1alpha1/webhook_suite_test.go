cca696a04dbe2ba4bec2467b9bf1770524c1a7af
kouki <kouworld0123@gmail.com>
2021-07-30 02:46:09 +0000

    adjusted manifest
    arranged cluster role
    fix label
    sample manifest use quay.io/cybozu/actions-controller
    update image tag at the time of release.
    
    Signed-off-by: kouki <kouworld0123@gmail.com>

--- api/v1alpha1/webhook_suite_test.go
+++ api/v1alpha1/webhook_suite_test.go
@@ -47,7 +47,7 @@ var _ = BeforeSuite(func() {
 		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "config", "crd", "bases")},
 		ErrorIfCRDPathMissing: false,
 		WebhookInstallOptions: envtest.WebhookInstallOptions{
-			Paths: []string{filepath.Join("..", "..", "config", "webhook")},
+			Paths: []string{filepath.Join("..", "..", "config", "controller", "webhook.yaml")},
 		},
 	}
 
