d6340f5f58262afeabbaf691b108ebf907f06766
kouki <kouworld0123@gmail.com>
2021-09-06 04:14:28 +0000

    fix func name
    Signed-off-by: kouki <kouworld0123@gmail.com>

--- controllers/secret_updater.go
+++ controllers/secret_updater.go
@@ -38,7 +38,7 @@ func (w SecretUpdater) Start(ctx context.Context) error {
 			return ctx.Err()
 		case <-ticker.C:
 			logger.Info("start secret watcher")
-			err := w.secretExpired(ctx)
+			err := w.secretUpdate(ctx)
 			if err != nil {
 				return err
 			}
@@ -46,7 +46,7 @@ func (w SecretUpdater) Start(ctx context.Context) error {
 	}
 }
 
-func (w SecretUpdater) secretExpired(ctx context.Context) error {
+func (w SecretUpdater) secretUpdate(ctx context.Context) error {
 	logger := log.FromContext(ctx).WithName("SecretUpdater")
 	rps := meowsv1alpha1.RunnerPoolList{}
 	err := w.client.List(ctx, &rps)
