fb3af2cfec37ed21636b8bc7dc7cda95dbdde0d7
Kenta Goto <kenta-goto@cybozu.co.jp>
2025-06-24 00:40:18 +0000

    Update webhook to use custom defaulter and validator
    Signed-off-by: Kenta Goto <kenta-goto@cybozu.co.jp>

--- api/v1alpha1/runnerpool_webhook.go
+++ api/v1alpha1/runnerpool_webhook.go
@@ -1,6 +1,9 @@
 package v1alpha1
 
 import (
+	"context"
+	"fmt"
+
 	constants "github.com/cybozu-go/meows"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	runtime "k8s.io/apimachinery/pkg/runtime"
@@ -11,44 +14,70 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
-func (r *RunnerPool) SetupWebhookWithManager(mgr ctrl.Manager) error {
+func SetupWebhookWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewWebhookManagedBy(mgr).
-		For(r).
+		For(&RunnerPool{}).
+		WithDefaulter(&RunnerPoolDefaulter{}).
+		WithValidator(&RunnerPoolValidator{}).
 		Complete()
 }
 
 // +kubebuilder:webhook:failurePolicy=fail,matchPolicy=equivalent,groups=meows.cybozu.com,resources=runnerpools,verbs=create,versions=v1alpha1,name=runnerpool-hook.meows.cybozu.com,path=/mutate-meows-cybozu-com-v1alpha1-runnerpool,mutating=true,sideEffects=none,admissionReviewVersions=v1
 
-var _ webhook.Defaulter = &RunnerPool{}
+type RunnerPoolDefaulter struct{}
 
-// Default implements webhook.Defaulter so a webhook will be registered for the type
-func (r *RunnerPool) Default() {
-	controllerutil.AddFinalizer(r, constants.RunnerPoolFinalizer)
+var _ webhook.CustomDefaulter = &RunnerPoolDefaulter{}
+
+// Default implements webhook.CustomDefaulter so a webhook will be registered for the type
+func (r *RunnerPoolDefaulter) Default(ctx context.Context, obj runtime.Object) error {
+	rp, ok := obj.(*RunnerPool)
+	if !ok {
+		return fmt.Errorf("expected a RunnerPool object but got %T", rp)
+	}
+
+	controllerutil.AddFinalizer(rp, constants.RunnerPoolFinalizer)
+	return nil
 }
 
 // +kubebuilder:webhook:failurePolicy=fail,matchPolicy=equivalent,groups=meows.cybozu.com,resources=runnerpools,verbs=create;update,versions=v1alpha1,name=runnerpool-hook.meows.cybozu.com,path=/validate-meows-cybozu-com-v1alpha1-runnerpool,mutating=false,sideEffects=none,admissionReviewVersions=v1
 
-var _ webhook.Validator = &RunnerPool{}
+type RunnerPoolValidator struct{}
 
-// ValidateCreate implements webhook.Validator so a webhook will be registered for the type
-func (r *RunnerPool) ValidateCreate() (warnings admission.Warnings, err error) {
-	errs := r.Spec.validateCreate()
+var _ webhook.CustomValidator = &RunnerPoolValidator{}
+
+// ValidateCreate implements webhook.CustomValidator so a webhook will be registered for the type
+func (r *RunnerPoolValidator) ValidateCreate(ctx context.Context, obj runtime.Object) (warnings admission.Warnings, err error) {
+	rp, ok := obj.(*RunnerPool)
+	if !ok {
+		return nil, fmt.Errorf("expected a RunnerPool object but got %T", rp)
+	}
+
+	errs := rp.Spec.validateCreate()
 	if len(errs) == 0 {
 		return nil, nil
 	}
-	return nil, apierrors.NewInvalid(schema.GroupKind{Group: GroupVersion.Group, Kind: "RunnerPool"}, r.Name, errs)
+	return nil, apierrors.NewInvalid(schema.GroupKind{Group: GroupVersion.Group, Kind: "RunnerPool"}, rp.Name, errs)
 }
 
-// ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
-func (r *RunnerPool) ValidateUpdate(old runtime.Object) (warnings admission.Warnings, err error) {
-	errs := r.Spec.validateUpdate(old.(*RunnerPool).Spec)
+// ValidateUpdate implements webhook.CustomValidator so a webhook will be registered for the type
+func (r *RunnerPoolValidator) ValidateUpdate(ctx context.Context, oldObj runtime.Object, newObj runtime.Object) (warnings admission.Warnings, err error) {
+	oldRp, ok := oldObj.(*RunnerPool)
+	if !ok {
+		return nil, fmt.Errorf("expected a RunnerPool object for the oldRp but got %T", oldRp)
+	}
+	newRp, ok := newObj.(*RunnerPool)
+	if !ok {
+		return nil, fmt.Errorf("expected a RunnerPool object for the newRp but got %T", newRp)
+	}
+
+	errs := newRp.Spec.validateUpdate(oldRp.Spec)
 	if len(errs) == 0 {
 		return nil, nil
 	}
-	return nil, apierrors.NewInvalid(schema.GroupKind{Group: GroupVersion.Group, Kind: "RunnerPool"}, r.Name, errs)
+	return nil, apierrors.NewInvalid(schema.GroupKind{Group: GroupVersion.Group, Kind: "RunnerPool"}, newRp.Name, errs)
 }
 
-// ValidateDelete implements webhook.Validator so a webhook will be registered for the type
-func (r *RunnerPool) ValidateDelete() (warnings admission.Warnings, err error) {
+// ValidateDelete implements webhook.CustomValidator so a webhook will be registered for the type
+func (r *RunnerPoolValidator) ValidateDelete(ctx context.Context, obj runtime.Object) (warnings admission.Warnings, err error) {
 	return nil, nil
 }
--- api/v1alpha1/webhook_suite_test.go
+++ api/v1alpha1/webhook_suite_test.go
@@ -89,7 +89,7 @@ var _ = BeforeSuite(func() {
 	})
 	Expect(err).NotTo(HaveOccurred())
 
-	err = (&RunnerPool{}).SetupWebhookWithManager(mgr)
+	err = SetupWebhookWithManager(mgr)
 	Expect(err).NotTo(HaveOccurred())
 
 	//+kubebuilder:scaffold:webhook
