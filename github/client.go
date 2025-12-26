020370babbed4e657efd953a93afc89f93d02345
012xx <night.fox.045@gmail.com>
2025-12-26 07:44:26 +0000

    update: ghinstallation/v2

--- github/client.go
+++ github/client.go
@@ -8,7 +8,7 @@ import (
 	"net/url"
 	"time"
 
-	"github.com/bradleyfalzon/ghinstallation"
+	"github.com/bradleyfalzon/ghinstallation/v2"
 	"github.com/google/go-github/v41/github"
 	"golang.org/x/oauth2"
 )
